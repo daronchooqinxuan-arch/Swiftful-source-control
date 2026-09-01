59e6829f4f743a4f8a87a8122cb2f8c654b05682
daronchooqinxuan-arch <daron_choo_qin_xuan@s2026.ssts.edu.sg>
2026-09-01 08:51:58 +0800

    Initial Commit

---
+++ Swiftful source control/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swiftful source control
+//
+//  Created by Daron Choo on 1/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swiftful source control/Swiftful_source_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftful_source_controlApp.swift
+//  Swiftful source control
+//
+//  Created by Daron Choo on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Swiftful_source_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
