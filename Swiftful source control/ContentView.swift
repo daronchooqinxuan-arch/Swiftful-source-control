bf0e5f202f2201e387db17f0a1aba7b283d868d7
daronchooqinxuan-arch <daron_choo_qin_xuan@s2026.ssts.edu.sg>
2026-09-02 16:58:30 +0800

    Add click me button

--- Swiftful source control/ContentView.swift
+++ Swiftful source control/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("Swiftful thinking!")
+            Button("Click me"){
+                
+            }
         }
         .padding()
     }
