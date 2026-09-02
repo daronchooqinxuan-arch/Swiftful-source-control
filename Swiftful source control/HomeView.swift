d15e6d19fd5c04055fad3e1622bccb362c80f9d0
daronchooqinxuan-arch <daron_choo_qin_xuan@s2026.ssts.edu.sg>
2026-09-02 17:44:09 +0800

    Add HomeView

---
+++ Swiftful source control/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Swiftful source control
+//
+//  Created by Daron Choo on 2/9/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
