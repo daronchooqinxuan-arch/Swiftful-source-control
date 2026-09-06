b13f69306779939924a28d35d7194264436deeae
daronchooqinxuan-arch <daron_choo_qin_xuan@s2026.ssts.edu.sg>
2026-09-06 15:46:20 +0800

    [Feature] Add two more text in HomeView

--- Swiftful source control/HomeView.swift
+++ Swiftful source control/HomeView.swift
@@ -6,9 +6,11 @@
 //
 
 import SwiftUI
-
 struct HomeView: View {
+    @State private var title: String = ""
     var body: some View {
+        Text("Screen 2")
+        Text("Screen 2")
         Text("Screen 2")
     }
 }
