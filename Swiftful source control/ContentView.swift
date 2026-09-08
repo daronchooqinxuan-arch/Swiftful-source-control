0a754e16e05693bc1d00fd735eb47449ce36c847
daronchooqinxuan-arch <daron_choo_qin_xuan@s2026.ssts.edu.sg>
2026-09-08 21:22:11 +0800

    [Clean] Updates to documention

--- Swiftful source control/ContentView.swift
+++ Swiftful source control/ContentView.swift
@@ -4,7 +4,14 @@
 //
 //  Created by Daron Choo on 1/9/26.
 //
-
+/*
+Clone = Copying the repo locally
+ Commit = save "Checkpoint" on our current branch
+ Stage = Prepare changes for a commit
+ Stash = save changes for later
+ Push = send local commits to remote repo
+ Pull = fetch remote commits to local repo
+ */
 import SwiftUI
 
 struct ContentView: View {
