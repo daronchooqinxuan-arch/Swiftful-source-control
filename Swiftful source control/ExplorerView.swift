1e160906dc2d5ecfa58d7bf2d9fa02e89912ae88
daronchooqinxuan-arch <daron_choo_qin_xuan@s2026.ssts.edu.sg>
2026-09-14 18:55:05 +0800

    [Feauture] Add ExplorerView

---
+++ Swiftful source control/ExplorerView.swift
@@ -0,0 +1,18 @@
+//
+//  ExplorerView.swift
+//  Swiftful source control
+//
+//  Created by Daron Choo on 14/9/26.
+//
+
+import SwiftUI
+
+struct ExplorerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExplorerView()
+}
